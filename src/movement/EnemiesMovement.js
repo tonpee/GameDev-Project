660f8216708cdb00ab59b9b897113fdbd8342ae4
Theerakan Choochat <tonpeech11@gmail.com>
2023-12-08 23:27:47 +0700

    tonpee edit time

--- src/movement/EnemiesMovement.js
+++ src/movement/EnemiesMovement.js
@@ -10,57 +10,45 @@ export function preload() {
   });
 }
 
-export function createEnemies() {
-    for (let i = 0; i <= 5; i++) {
-        create(this);
-    }
+export function create() {
+  const distance = 80;
+  let randomXForEll = Phaser.Math.Between(0 + distance, 1280 - distance);
+  this.ell = this.physics.add
+    .sprite(randomXForEll, 1280, "ell")
+    .setScale(2)
+    .setSize(18, 35)
+    .setOffset(16, 10);
+  this.ell.rotation = Phaser.Math.DegToRad(270);
+  this.ell.anims.create({
+    key: "ell-attack",
+    frames: this.anims.generateFrameNumbers("ell", {
+      start: 0,
+      end: 5,
+    }),
+    frameRate: 6,
+    repeat: -1,
+  });
+  this.ell.setVelocityY(-300);
+
+  let randomYForSail = Phaser.Math.Between(720 - distance, 0 + distance);
+  this.sail = this.physics.add // นำ `sail.physics.add` ออก
+    .sprite(100, randomYForSail, "sail")
+    .setScale(2)
+    .setSize(40, 15)
+    .setOffset(2, 18);
+  this.sail.anims.create({
+    key: "sail-attack",
+    frames: this.anims.generateFrameNumbers("sail", {
+      start: 0,
+      end: 5,
+    }),
+    frameRate: 6,
+    repeat: -1,
+  });
+  this.sail.setVelocityX(300);
 }
 
-let ell;
-let sail;
-
-export function create(obj) {
-    const distance = 80;
-    console.log(obj);
-    let randomXForEll = Phaser.Math.Between(0 + distance, 1280 - distance);
-    ell = obj.physics.add
-      .sprite(randomXForEll, 1280, "ell")
-      .setScale(2)
-      .setSize(18, 35)
-      .setOffset(16, 10);
-    ell.rotation = Phaser.Math.DegToRad(270);
-    obj.anims.create({
-      key: "ell-attack",
-      frames: obj.anims.generateFrameNumbers("ell", {
-        start: 0,
-        end: 5,
-      }),
-      frameRate: 6,
-      repeat: -1,
-    });
-    ell.setVelocityY(-300);
-    
-    let randomYForSail = Phaser.Math.Between(720 - distance, 0 + distance);
-    sail = obj.physics.add
-      .sprite(100, randomYForSail, "sail")
-      .setScale(2) // Fixed this line
-      .setSize(40, 15)
-      .setOffset(2, 18);
-    obj.anims.create({
-      key: "sail-attack",
-      frames: obj.anims.generateFrameNumbers("sail", {
-        start: 0,
-        end: 5,
-      }),
-      frameRate: 6,
-      repeat: -1,
-    });
-    sail.setVelocityX(300);
-  }
-  
-
 export function update() {
-  ell.anims.play("ell-attack", true);
-
-  sail.anims.play("sail-attack", true);
+  this.ell.anims.play("ell-attack", true);
+  this.sail.anims.play("sail-attack", true);
 }
--- src/scenes/JellyGame.js
+++ src/scenes/JellyGame.js
@@ -16,11 +16,22 @@ class JellyGame extends Phaser.Scene {
   create() {
     this.bg = this.add.tileSprite(0, 0, 768, 192, "background").setOrigin(0, 0).setDepth(-999).setScale(4);
     PlayerMovement.create.call(this);
-    EnemiesMovement.createEnemies.call(this);
+    this.startTime = this.time.now;
+
+    EnemiesMovement.create.call(this);
   }
   update() {
     PlayerMovement.update.call(this);
     EnemiesMovement.update.call(this);
+    const elapsedTime = this.time.now - this.startTime;
+    //show time in milliseconds
+    if (elapsedTime % 1000 === 0) {
+        console.log(elapsedTime);
+    }
+
+    if (elapsedTime % 6500 === 0) {
+        EnemiesMovement.create.call(this);
+    }
   }
 }
 
